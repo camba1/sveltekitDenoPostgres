5a44e210f9b17e3cf481f54c842b343a56249a11
Juan Peredo <jperedo@bolbeck.com>
2022-06-27 22:51:45 -0500

    Add connection to the DB

--- deno/deps.ts
+++ deno/deps.ts
@@ -1,1 +1,2 @@
 export {serve} from "https://deno.land/std@0.145.0/http/mod.ts";
+export { Pool } from "https://deno.land/x/postgres@v0.16.1/mod.ts";
--- deno/main.ts
+++ deno/main.ts
@@ -1,8 +1,79 @@
-import { serve } from "./deps.ts";
+import { serve, Pool } from "./deps.ts";
 
-function handler(req: Request): Response {
-  return new Response("Hello, World!");
+const databaseUrl = Deno.env.get("DATABASE_URL")!;
+
+
+const pool = new Pool(databaseUrl, 3, true);
+
+
+const connection = await pool.connect();
+try {
+
+  await connection.queryObject`
+    CREATE TABLE IF NOT EXISTS todos (
+      id SERIAL PRIMARY KEY,
+      title TEXT NOT NULL
+    )
+  `;
+} finally {
+
+  connection.release();
+}
+
+async function handler(req: Request): Promise<Response> {
+  const url = new URL(req.url);
+  if (url.pathname !== "/todos") {
+    return new Response("Not Found", { status: 404 });
+  }
+
+
+  const connection = await pool.connect();
+
+  try {
+    switch (req.method) {
+      case "GET": {
+
+        const result = await connection.queryObject`
+          SELECT * FROM todos
+        `;
+
+
+        const body = JSON.stringify(result.rows, null, 2);
+
+
+        return new Response(body, {
+          headers: { "content-type": "application/json" },
+        });
+      }
+      case "POST": {
+
+
+        const title = await req.json().catch(() => null);
+        if (typeof title !== "string" || title.length > 256) {
+          return new Response("Bad Request", { status: 400 });
+        }
+
+
+        await connection.queryObject`
+          INSERT INTO todos (title) VALUES (${title})
+        `;
+
+
+        return new Response("", { status: 201 });
+      }
+      default:
+        return new Response("Method Not Allowed", { status: 405 });
+    }
+  } catch (err) {
+    console.error(err);
+
+    return new Response(`Internal Server Error\n\n${err.message}`, {
+      status: 500,
+    });
+  } finally {
+
+    connection.release();
+  }
 }
 
 serve(handler, {port: 4242});
-
